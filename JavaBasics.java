45e18974dc36702d341003e8425e2db06f933647
maurash01 <ashumaurya023@gmail.com>
2024-07-14 02:03:00 +0530

    Basics of  Java

---
+++ JavaBasics.java
@@ -0,0 +1,15 @@
+public class JavaBasics{
+    public static void main(String args[]){
+        System.out.print("Hello World");
+        System.out.print("1 2 3 4 5 "); 
+        // This will print in Single line 
+
+        System.out.println("Hello World "); //println - Print k baad 1 line ka space 
+        System.out.println("1 2 3 4 5 "); 
+        System.out.print("10 20 30  \n "); 
+        System.out.println("15 30 45  "); // \n khi bhi line gap k liye 
+    
+
+    }
+}
+//boilerplatecode
